2224ff2423c17debd7f32ee1fc901c4585472c92
Jesus Vasquez <jesusvasquez333@gmail.com>
2021-01-20 09:47:32 -0800

    Add initial version of the top bash script

---
+++ test-rtm.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+# Import functions
+. ./scripts/functions.sh
+
+#############
+# Main body #
+#############
+
+# Parse the inputs arguments.
+arg_parser
+
+# Get FPGA IP address
+getFpgaIpAddr
+
+# Firmware version checking
+checkFW
+
+if [ -z "${manual}" ]; then
+    echo "using automatic test mode"
+else
+    echo "Using manual test mode"
+fi
