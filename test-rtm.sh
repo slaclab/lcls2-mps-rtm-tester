3f271c158912542c2504c6aef3f68fd5c560ebad
Jesus Vasquez <jesusvasquez333@gmail.com>
2021-01-20 18:39:55 -0800

    Move the definition to the top level script

--- test-rtm.sh
+++ test-rtm.sh
@@ -1,5 +1,30 @@
 #!/usr/bin/env bash
 
+###############
+# Definitions #
+###############
+
+# TOP directory, replacing the slac.stanford.edu soft link by slac
+# which is not always present in the linuxRT CPUs
+top_dir=$(dirname -- "$(readlink -f $0)" | sed 's/slac.stanford.edu/slac/g')
+
+# This script name
+script_name=$(basename $0)
+
+# Shell PID
+top_pid=$$
+
+# Firmware file location
+fw_top_dir="./firmware/ATCA/"
+
+# Trap TERM signals and exit
+trap "echo 'An ERROR was found. Check shelf manager & card state! Aborting...'; exit 1" TERM
+
+
+########################
+# Function definitions #
+########################
+
 # Import functions
 . ./bash/functions.sh
 
