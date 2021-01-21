a5bde9d637667645da087bd5435ac476d796a988
Jesus Vasquez <jvasquez@slac.stanford.edu>
2021-01-20 20:07:03 -0800

    Calculate the path to the TOP YAML file

--- scripts/automatic-timing-test.py
+++ scripts/automatic-timing-test.py
@@ -1,10 +1,16 @@
 #!/usr/bin/env python3
 
+import os
+import sys
 
 from RtmTester.CpswRoot import CpswRoot
 
+# Calculate path to TOP YAML file
+top_dir = os.path.dirname(sys.argv[0])
+yaml_file = f'{top_dir}/../firmware/ATCA/AmcCarrierMpsAnalogLinkNode_project.yaml/000TopLevel.yaml'
+
 # Create the root device
-r = CpswRoot(yaml_file="")
+r = CpswRoot(yaml_file=yaml_file)
 
 # Set LCLS1 mode timing, and check if the link is up
 print("Testing LCLS1 mode timing...  ")
