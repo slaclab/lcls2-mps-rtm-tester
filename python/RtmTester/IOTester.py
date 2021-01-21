6bf3a862c0e4083c51b2034680a972c4dd54190d
Jesus Vasquez <jesusvasquez333@gmail.com>
2021-01-21 11:34:17 -0800

    Add missing argument

--- python/RtmTester/IOTester.py
+++ python/RtmTester/IOTester.py
@@ -5,13 +5,14 @@ class AutomaticIOTester():
     Test the RTM I/O channels, using the tester device.
     """
 
-    def __init__(self, root, ip_addr):
+    def __init__(self, root, ip_addr, port_number):
         """
         initialize the object.
         """
 
         self.root = root
         self.ip_addr = ip_addr
+        self.port_number = port_number
 
     def run_tests(self):
         """
