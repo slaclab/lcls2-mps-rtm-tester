d89a5986155f4eac49b69c2e27a1509f22c6a5ac
Jesus Vasquez <jesusvasquez333@gmail.com>
2021-01-20 17:51:25 -0800

    Move the SocketHanlder as an inner class to TesterDevice, as it is not used anywhere else

--- python/RtmTester/TesterDevice.py
+++ python/RtmTester/TesterDevice.py
@@ -3,73 +3,20 @@
 import socket
 
 
-class SocketHanlder:
-    """
-    Class to handler the TCP socket.
-    It provides a method to send a message and read back the response.
-    """
-    def __init__(self, ip_addr, port_number, terminator='@'):
-        self.ip_addr = ip_addr
-        self.port_number = port_number
-        self.terminator = terminator
-        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-        try:
-            self.socket.connect((ip_addr, port_number))
-        except socket.error as e:
-            print("ERROR: Can not connect to target: {}".format(e))
-            raise
-
-        print("Connected to {}.{}".format(ip_addr, port_number))
-
-    # Send message and read response
-    def send(self, msg):
-
-        send_size = len(msg)
-        total_sent = 0
-
-        # Send command
-        while total_sent < send_size:
-            sent = self.socket.send(msg)
-
-            # Check if connection is broken
-            if sent == 0:
-                raise RuntimeError("Socket connection broken")
-
-            # Keep sending until all message is sent
-            total_sent = total_sent + sent
-
-        # Read response
-        response = ""
-        while True:
-            r = self.socket.recv(1024)
-
-            # Check if connection is broken
-            if r == b'':
-                raise RuntimeError("Socket connection broken")
-
-            response += r
-
-            # Keep reading until terminator is found
-            if r.endswith(self.terminator):
-                # Remove terminator from response
-                response = response[:-1]
-                break
-
-        return response
-
-
 class TesterDevice:
     """
     Class to communicate with the target tester device.
     It provides methods to write the outputs, read the inputs and
     get the device information.
     """
-    def __init__(self, ip_addr, port_number):
-        self.socket = SocketHanlder(ip_addr, port_number)
 
-    # Generic method used to send a command and process the response.
+    def __init__(self, ip_addr, port_number):
+        self.socket = self.SocketHanlder(ip_addr, port_number)
+
     def sendCommand(self, command):
+        """
+        Generic method used to send a command and process the response.
+        """
 
         # Add terminator
         command += '\n'
@@ -84,17 +31,88 @@ class TesterDevice:
         # return the response message without the error code.
         return r[1:]
 
-    # Command to write the outputs
     def writeOutputs(self, val):
+        """
+        Command to write the outputs.
+        """
+
         self.sendCommand('=' + str(val))
 
-    # Command to read the inputs
     def readInputs(self):
+        """
+        Command to read the inputs.
+        """
+
         try:
             return int(self.sendCommand('?'))
         except ValueError:
             raise RuntimeError("Not-numeric value received")
 
-    # Command to get the device information
     def readInfo(self):
+        """
+        Command to get the device information.
+        """
+
         return self.sendCommand('i')
+
+    class SocketHanlder:
+        """
+        Inner class to handler the TCP socket.
+        It provides a method to send a message and read back the response.
+        """
+
+        def __init__(self, ip_addr, port_number, terminator='@'):
+            """
+            Initialize the object.
+            """
+
+            self.ip_addr = ip_addr
+            self.port_number = port_number
+            self.terminator = terminator
+            self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+            try:
+                self.socket.connect((ip_addr, port_number))
+            except socket.error as e:
+                print("ERROR: Can not connect to target: {}".format(e))
+                raise
+
+            print("Connected to {}.{}".format(ip_addr, port_number))
+
+        def send(self, msg):
+            """
+            Send message and read response.
+            """
+
+            send_size = len(msg)
+            total_sent = 0
+
+            # Send command
+            while total_sent < send_size:
+                sent = self.socket.send(msg)
+
+                # Check if connection is broken
+                if sent == 0:
+                    raise RuntimeError("Socket connection broken")
+
+                # Keep sending until all message is sent
+                total_sent = total_sent + sent
+
+            # Read response
+            response = ""
+            while True:
+                r = self.socket.recv(1024)
+
+                # Check if connection is broken
+                if r == b'':
+                    raise RuntimeError("Socket connection broken")
+
+                response += r
+
+                # Keep reading until terminator is found
+                if r.endswith(self.terminator):
+                    # Remove terminator from response
+                    response = response[:-1]
+                    break
+
+            return response
