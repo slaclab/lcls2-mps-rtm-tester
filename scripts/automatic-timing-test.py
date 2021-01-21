d00204814434a20de4fe129f403ef0cf684bd8a0
Jesus Vasquez <jvasquez@slac.stanford.edu>
2021-01-20 20:19:54 -0800

    Fix PEP8 warning

--- scripts/automatic-timing-test.py
+++ scripts/automatic-timing-test.py
@@ -7,7 +7,8 @@ from RtmTester.CpswRoot import CpswRoot
 
 # Calculate path to TOP YAML file
 top_dir = os.path.dirname(sys.argv[0])
-yaml_file = f'{top_dir}/../firmware/ATCA/AmcCarrierMpsAnalogLinkNode_project.yaml/000TopLevel.yaml'
+yaml_file = f'{top_dir}/../firmware/ATCA/' \
+             'AmcCarrierMpsAnalogLinkNode_project.yaml/000TopLevel.yaml'
 
 # Create the root device
 r = CpswRoot(yaml_file=yaml_file)
