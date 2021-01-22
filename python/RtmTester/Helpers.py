a6ec4422e2ae7afd8c95d442d2e32523c16cc364
Jesus Vasquez <jesusvasquez333@gmail.com>
2021-01-22 15:03:11 -0800

    Fix indentations

--- python/RtmTester/Helpers.py
+++ python/RtmTester/Helpers.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python3
 
 def print_ok(msg, **kargs):
-	"""
-	Print the message in green.
-	"""
+    """
+    Print the message in green.
+    """
     print(f"\033[32m{msg}\033[0m", **kargs)
 
 
 def print_failed(msg, **kargs):
-	"""
-	Print the message in red.
-	"""
+    """
+    Print the message in red.
+    """
     print(f"\033[31m{msg}\033[0m", **kargs)
